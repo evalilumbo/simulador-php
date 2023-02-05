8790513ac3ccbf2dab2949de54566ec4b352eb45
Evaristo Lilumbo <118283471+evalilumbo@users.noreply.github.com>
2023-02-05 13:58:00 +0100

    Update pagina.php

--- layouts/pagina.php
+++ layouts/pagina.php
@@ -1,17 +1,49 @@
-<!doctype html>
-<html lang="pt">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Hello, world!</title>
-  </head>
-  <body> 
-    
-    
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-   </body>
-</html>
+<div class="accordion mb-2 headers">
+   <div class="row">
+      <div class="col">
+         <div class="bbb">Banco Santander</div>
+      </div>
+      <div class="col">
+         R$ <?= number_format($jsons[0]->saldo,2,",","."); ?>
+      </div>
+      <div class="col">
+         R$ <?= number_format($jsons[1]->prestacao*100/40,2,",","."); ?>
+      </div>
+      <div class="col">
+         R$ <?= number_format($jsons[1]->prestacao,2,",","."); ?>
+      </div>
+      <div class="col">
+         <?= $jsons[0]->taxa; ?>% a.a.
+      </div>
+      <div class="col"> 
+         <?= $jsons[0]->prazo; ?> meses
+      </div>
+      <div class="col"> 
+         <?= $cet; ?>% a.a.
+      </div>
+   </div>
+</div>
+<table id="tabela1" class="display" style="width:100%">
+   <thead>
+      <tr>
+         <th>Parcela</th>
+         <th>Amortização</th>
+         <th>Juros</th>
+         <th>Seguros + TAC</th>
+         <th>Prestação</th>
+         <th>Saldo</th>
+      </tr>
+   </thead>
+   <tbody>
+      <?php foreach($dados as $v){ ?>
+      <tr>
+         <td><?= $v['parcela']; ?></td>
+         <td>R$ <?= number_format($v['amortizacao'],2,",","."); ?></td>
+         <td>R$ <?= number_format($v['juros'],2,",","."); ?></td>
+         <td>R$ <?= number_format($v['mip'] + $v['dfi'] + $v['tac'],2,",","."); ?></td>
+         <td>R$ <?= number_format($v['prestacao'],2,",","."); ?></td>
+         <td>R$ <?= number_format($v['saldo'],2,",","."); ?></td>
+      </tr>
+      <?php } ?>
+   <tbody>	
+</table>
